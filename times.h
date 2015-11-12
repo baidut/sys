03b2291947380896392e2ade678f3c51b8cafb05
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2015-11-12 09:55:38 +0800

    Automatic switching by conditional compilation

--- times.h
+++ times.h
@@ -3,40 +3,44 @@
 #ifndef _TIMES_H
 #define _TIMES_H
 
-#ifdef _WIN32
-#include <sys/timeb.h>
-#include <sys/types.h>
-#include <winsock2.h>
-#include <windows.h> // sleep
+#ifdef __linux__
+    #include <sys/times.h>
 
-int gettimeofday(struct timeval* t,void* timezone);
-#define sleep(sleeptime) Sleep(1000*(sleeptime)) // linux sleep sleeptime s while windows sleep sleeptime ms
+#elif _WIN32
+	#include <sys/timeb.h>
+	#include <sys/types.h>
+	#include <winsock2.h>
+	#include <windows.h> // sleep
 
-// from linux's sys/times.h
+	int gettimeofday(struct timeval* t,void* timezone);
+	#define sleep(sleeptime) Sleep(1000*(sleeptime)) // linux sleep sleeptime s while windows sleep sleeptime ms
 
-//#include <features.h>
+	// from linux's sys/times.h
 
-#define __need_clock_t
-#include <time.h>
+	//#include <features.h>
+
+	#define __need_clock_t
+	#include <time.h>
 
 
-/* Structure describing CPU time used by a process and its children.  */
-struct tms
-  {
-    clock_t tms_utime;          /* User CPU time.  */
-    clock_t tms_stime;          /* System CPU time.  */
+	/* Structure describing CPU time used by a process and its children.  */
+	struct tms
+	  {
+		clock_t tms_utime;          /* User CPU time.  */
+		clock_t tms_stime;          /* System CPU time.  */
 
-    clock_t tms_cutime;         /* User CPU time of dead children.  */
-    clock_t tms_cstime;         /* System CPU time of dead children.  */
-  };
+		clock_t tms_cutime;         /* User CPU time of dead children.  */
+		clock_t tms_cstime;         /* System CPU time of dead children.  */
+	  };
 
-/* Store the CPU time used by this process and all its
-   dead children (and their dead children) in BUFFER.
-   Return the elapsed real time, or (clock_t) -1 for errors.
-   All times are in CLK_TCKths of a second.  */
-clock_t times (struct tms *__buffer);
+	/* Store the CPU time used by this process and all its
+	   dead children (and their dead children) in BUFFER.
+	   Return the elapsed real time, or (clock_t) -1 for errors.
+	   All times are in CLK_TCKths of a second.  */
+	clock_t times (struct tms *__buffer);
 
-typedef long long suseconds_t ;
+	typedef long long suseconds_t ;
 
 #endif
-#endif
+
+#endif // #ifndef _TIMES_H
